62377f2781795a084d8158b888564ea72e450769
OlegLyzwinski <lyzwinskioleg@gmail.com>
2025-02-23 13:49:31 +0100

    cleaned repository

--- Card.py
+++ Card.py
@@ -1,7 +1,19 @@
-class Card:
+'''class Card:
     def __init__(self, image, box, center):
         self.image = image
         self.box = box
         self.center = center
         self.color = None
-        self.symbol = None
+        self.symbol = None'''
+
+from dataclasses import dataclass
+import numpy as np
+from typing import List, Optional, Union
+
+@dataclass
+class Card:
+    image: np.ndarray                       # Macierz przechowujaca karte (RGB)
+    box: List[int]                          # Lista 4 liczb (np. [x1, y1, x2, y2])
+    center: np.ndarray  # Środek jako macierz lub lista współrzędnych [x, y]
+    color: Optional[str] = None             # Kolor, np. "czerwony", domyślnie brak
+    symbol: Optional[str] = None            # Symbol (np. "1"), domyślnie brak
